649e0742999669f020a2bea131fed35df6f8ef16
Joshua Zimmerman <jpz6926@gmail.com>
2025-02-17 22:16:54 -0500

    Base commit, adds basic application for encrypting and decrypting user input

---
+++ SampleAesCryptography/AesCryptographyHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Cryptography;
+
+namespace SampleAesCryptography
+{
+    public static class AesCryptographyHelper
+    {
+        public static Aes AesCryptography { get; } = Aes.Create();
+
+        public static byte[] EncryptString(string inputText)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(inputText))
+                {
+                    throw new ArgumentNullException("inputText");
+                }
+
+                var encryptor = AesCryptography.CreateEncryptor(AesCryptography.Key, AesCryptography.IV);
+                using (var memoryStreamEncrypt = new MemoryStream())
+                {
+                    using (var cryptoStreamEncrypt = new CryptoStream(memoryStreamEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var streamWriterEncrypt = new StreamWriter(cryptoStreamEncrypt))
+                        {
+                            streamWriterEncrypt.Write(inputText);
+                        }
+                    }
+                    return memoryStreamEncrypt.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"EncryptString() error: {ex.Message}");
+                return [];
+            }
+        }
+
+        public static string DecryptBytesToString(byte[] inputBytes)
+        {
+            var decryptor = AesCryptography.CreateDecryptor(AesCryptography.Key, AesCryptography.IV);
+            var decryptedText = "";
+
+
+            try
+            {
+                using (var memoryStreamDecrypt = new MemoryStream(inputBytes))
+                {
+                    using (var cryptoStreamDecrypt = new CryptoStream(memoryStreamDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReaderDecrypt = new StreamReader(cryptoStreamDecrypt))
+                        {
+                            decryptedText = streamReaderDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+                return decryptedText;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"DecryptBytesToString() exception: {ex.Message}");
+                return String.Empty;
+            }
+        }
+    }
+}
---
+++ SampleAesCryptography/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SampleAesCryptography
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a string to encrypt: " );
+            var inputText = Console.ReadLine();
+
+            while (String.IsNullOrEmpty(inputText))
+            {
+                Console.WriteLine("Didn't detect a valid input, please try again");
+                inputText = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your input text was \"{inputText}\"");
+
+            Console.WriteLine("Encrpyting...");
+            var encryptedBytes = AesCryptographyHelper.EncryptString(inputText);
+            Console.WriteLine($"Encrypted bytes are: {System.Text.Encoding.UTF8.GetString(encryptedBytes)}");
+
+            var decryptedBytes = AesCryptographyHelper.DecryptBytesToString(encryptedBytes);
+            Console.WriteLine($"Decrypted string is: {decryptedBytes}");
+
+        }
+    }
+}
